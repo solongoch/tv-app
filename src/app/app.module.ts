8a009d8d7fe331959daca77f89e0a43d1ccd9b7f
Lakshmipriya <slpriya.as@gmail.com>
2020-05-14 11:11:15 -0700

    Episode Component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,19 +7,22 @@ import { ShowDataService } from './show-data.service';
 import { HttpClientModule } from '@angular/common/http';
 import { SeasonsListComponentComponent } from './seasons-list-component/seasons-list-component.component';
 import { SeasonsListServiceService } from './seasons-list-service.service';
+import { ShowEpisodesListComponent } from './show-episodes-list/show-episodes-list.component';
+import { EpisodeServiceService } from './episode-service.service';
 
 @NgModule({
   declarations: [
     AppComponent,
     ShowSingleSearchComponent,
-    SeasonsListComponentComponent
+    SeasonsListComponentComponent,
+    ShowEpisodesListComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule
   ],
-  providers: [ShowDataService,SeasonsListServiceService],
+  providers: [ShowDataService,SeasonsListServiceService,EpisodeServiceService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
